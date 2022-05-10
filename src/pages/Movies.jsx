f64617ace36f831dacff6c4b73a1407a2b66296d
Antonio <tonicintora@gmail.com>
2022-05-10 20:19:51 +0200

    user required in movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Movie from "../components/Movie";
 import axios from "axios";
-import Footer from "../components/Footer";
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
+  const { user } = useContext(AuthContext);
+  if (!user) {
+    return window.location.replace("/");
+  }
   useEffect(() => {
     const getMovies = async () => {
       try {
@@ -27,11 +30,13 @@ const Movies = () => {
     <div className="w-full min-h-screen bg-zinc-900 overflow-hidden">
       <Navbar activeMenu={true} menu="movies" />
       <div className="w-screen h-full bg-zinc-900">
-        <h1 className="text-white font-semibold text-4xl py-4 px-8">Movies</h1>
-        <div className="w-screen grid justify-center items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-5 mx-4 gap-4">
-          {movies.map((movie) => (
-            <Movie data={movie} />
-          ))}
+        <h1 className="text-white font-semibold text-4xl py-4 px-8">Animes</h1>
+        <div className="w-screen grid justify-center items-center grid-cols-1 md:grid-cols-4 lg:grid-cols-5 mx-4 gap-4">
+          {
+            movies.map((movie) => (
+                <Movie data={movie} />
+            ))
+          }
         </div>
       </div>
       <Footer />
